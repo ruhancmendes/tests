59b99845172a76096874488aa74866971a168abb
ruhancmendes <ruhancmendes@gmail.com>
2025-09-17 17:59:06 -0300

    beforeAll

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,18 +4,25 @@ import { sum } from "./server";
 
 describe("sum function", () => {
     //agrupamento de testes
-    
-test("sum of 3 + 7 must be 10", () => {
+
+    let sumResult: number //variável no escopo para todas as funções de teste terão acesso
+
+    beforeAll(() => {
+        //executa uma vez antes de todos os testes
+        sumResult = 10
+    })
+
+    it("should do sum of 3 + 7 must be 10", () => {
     const result = sum(7, 3);
 
     expect(result).toBe(10); //verifica se o resultado é como o esperado
-})
+    })
 
-test("sum of 2 + 2 must be 4", () => {
+    it("should do sum of 2 + 2 must be 4", () => {
     const result = sum(2, 2);
 
     expect(result).toBe(4); //verifica se o resultado é como o esperado
-})
+    })
 
 })
 
