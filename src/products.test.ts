8dc28709eb797e2e3c3ba1f481e8896c716d5817
ruhancmendes <ruhancmendes@gmail.com>
2025-09-17 18:21:43 -0300

    test E2E

---
+++ src/products.test.ts
@@ -0,0 +1,11 @@
+// teste 
+import request from "supertest"
+import { app } from "./app"
+
+
+describe("products", () => {
+   it("should list products", async () => {
+    const response = await request(app).get("/products")
+    console.log(response.body)
+   })
+})
