c630ffc1e5c235fde8aa8d6c01d3aa43fafbdbb5
ruhancmendes <ruhancmendes@gmail.com>
2025-09-17 17:48:06 -0300

    agrupamento de testes

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -2,8 +2,20 @@
 
 import { sum } from "./server";
 
+describe("sum function", () => {
+    //agrupamento de testes
+    
 test("sum of 3 + 7 must be 10", () => {
     const result = sum(7, 3);
 
     expect(result).toBe(10); //verifica se o resultado é como o esperado
 })
+
+test("sum of 2 + 2 must be 4", () => {
+    const result = sum(2, 2);
+
+    expect(result).toBe(4); //verifica se o resultado é como o esperado
+})
+
+})
+
